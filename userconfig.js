1bc37765cf678d7a7d0a675ee902858d8321142c
Dekoder-py <kyleyesmariam@icloud.com>
2025-05-28 21:53:43 +1200

    fix school i think

--- userconfig.js
+++ userconfig.js
@@ -114,7 +114,7 @@ const default_configuration = {
       ],
     },
     {
-      name: "SCHOOL",
+      name: "sch ool",
       background_url: "src/img/banners/banner_14.gif",
       categories: [
         {
